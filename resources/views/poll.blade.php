26e8bdbb59e3d0e638539fa5dba06ab36a18f525
Cauê Pereira <cauecvx@gmail.com>
2026-01-01 17:28:32 -0300

    Fixed page titles

--- resources/views/poll.blade.php
+++ resources/views/poll.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Poll</title>
+    <title>Enquetes</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
